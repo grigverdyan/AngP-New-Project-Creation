a37d8adfa63b5a4a95a58019b015b94fb1790399
Grigori Verdyan <Grigori.Verdyan@synisys.com>
2021-12-27 16:36:25 +0400

    delted items

--- world-food-program/src/app/app.component.ts
+++ world-food-program/src/app/app.component.ts
@@ -13,31 +13,6 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{    // implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterViewInit, AfterContentChecked, OnDestroy
+export class AppComponent{
   title = 'world-food-program';
-
-  // constructor() {
-  //   console.log("constructor");
-  // }
-  // ngOnInit() {
-  //   console.log("OnInit")
-  // }
-  // ngOnChanges(changes: SimpleChanges) {
-  //   console.log("OnChanges");
-  // }
-  // ngDoCheck() {
-  //   console.log("DoCheck")
-  // }
-  // ngAfterContentInit() {
-  //   console.log("AfterContentInit")
-  // }
-  // ngAfterContentChecked() {
-  //   console.log("AfterContentCjecked")
-  // }
-  // ngAfterViewInit() {
-  //   console.log("AfterViewInit")
-  // }
-  // ngOnDestroy() {
-  //   console.log("OnDestroy")
-  // }
 }
